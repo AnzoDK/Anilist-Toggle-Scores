294d058e42ef28bc8ee6a26372dc54d83a04d21f
Anzo <antonr@live.dk>
2025-10-21 15:33:03 +0000

    Add toggle button to hide/show Anilist scores

---
+++ toggle_anilist_scores.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Toggle Anilist Scores Script
+// @namespace   Violentmonkey Scripts
+// @match       https://anilist.co/search/*
+// @grant       none
+// @version     1.0
+// @author      AnzoDK
+// @description 21/10/2025, 13.21.43
+// ==/UserScript==
+var styleSheetRuleIndex = -1;
+function toggleScores()
+{
+  if(styleSheetRuleIndex != -1)
+  {
+    document.styleSheets[0].removeRule(styleSheetRuleIndex);
+    styleSheetRuleIndex = -1;
+    return;
+  }
+  var index = document.styleSheets[0].insertRule(".score {display:none!important;}",document.styleSheets[0].rules.length);
+  styleSheetRuleIndex = index;
+}
+var btn = document.createElement("button");
+btn.id = "toggleScoresBtn";
+btn.appendChild(document.createTextNode("Toggle Scores!!"));
+btn.onclick = toggleScores
+btn.style.backgroundColor = "#3db4f2";
+btn.style.color = "white";
+btn.style.borderRadius = "6px";
+document.getElementsByClassName("filters")[0].appendChild(btn);
+
+
