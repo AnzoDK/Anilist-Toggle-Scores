63183b78f46e92b4a66e6de720054858c3f77d1b
Anzo <antonr@live.dk>
2026-03-15 17:12:14 +0000

    Update version and refactor toggle button initialization

--- toggle_anilist_scores.js
+++ toggle_anilist_scores.js
@@ -3,13 +3,14 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://anilist.co/search/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.0
 // @author      AnzoDK
-// @homepageURL https://github.com/AnzoDK/Anilist-Toggle-Scores
+// @license     MIT
 // @downloadURL https://github.com/AnzoDK/Anilist-Toggle-Scores/releases/latest/download/toggle_anilist_scores.js
 // @description 21/10/2025, 13.21.43
 // ==/UserScript==
 var styleSheetRuleIndex = -1;
+var targetNode = document.getElementsByClassName("filters")[0];
 function toggleScores()
 {
   if(styleSheetRuleIndex != -1)
@@ -30,10 +31,37 @@ function createToggleButton()
   btn.style.backgroundColor = "#3db4f2";
   btn.style.color = "white";
   btn.style.borderRadius = "6px";
-  document.getElementsByClassName("filters")[0].appendChild(btn);
+  targetNode.appendChild(btn);
+
 }
 function Init_ScoreToggle()
 {
   createToggleButton();
+  console.log("Toggle Anilist Scores: Button initilized");
 }
-Init_ScoreToggle();
+
+function CheckForTarget()
+{
+  for (const mutation of mutationList)
+    {
+      if(document.getElementsByClassName("filters")[0])
+        {
+          targetNode = document.getElementsByClassName("filters")[0];
+          Init_ScoreToggle();
+          observer.disconnect();
+          return;
+        }
+    }
+}
+
+const mo = new MutationObserver(CheckForTarget);
+if(!targetNode)
+  {
+    console.log("Toggle Anilist Scores: Failed to locate filters - Observing page for changes...")
+    mo.observe(document.body, { attributes: false, childList: true, subtree: true });
+  }
+else
+  {
+    Init_ScoreToggle();
+  }
+
