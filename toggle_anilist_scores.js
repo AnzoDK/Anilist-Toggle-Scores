eb8b5842f53e5ba70310af6d76f75c7e844de293
AnzoDK <anzo@nepu.dk>
2026-04-21 16:57:25 +0200

    made toggling persist

--- toggle_anilist_scores.js
+++ toggle_anilist_scores.js
@@ -3,24 +3,32 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://anilist.co/search/*
 // @grant       none
-// @version     1.1.2
+// @version     1.2.0
 // @author      AnzoDK
 // @license     MIT
 // @downloadURL https://github.com/AnzoDK/Anilist-Toggle-Scores/releases/latest/download/toggle_anilist_scores.js
 // @description 21/10/2025, 13.21.43
 // ==/UserScript==
-var state = false;
+var state = localStorage.getItem("anilist_toggleScoresState") === "true" ? true : false;
 var styleNode = null;
 var targetNode = document.getElementsByClassName("filters")[0];
+var toggleBtn = null;
+function toggleScoresAndSaveState()
+{
+  toggleScores();
+  localStorage.setItem("anilist_toggleScoresState", state);
+}
 function toggleScores()
 {
   if(state)
   {
     styleNode.innerHTML = "";
     state = false;
+    toggleBtn.style.backgroundColor = "#182a34";
     return;
   }
   styleNode.innerHTML = ".score {display:none!important;}";
+  toggleBtn.style.backgroundColor = "#3db4f2";
   state = true;
 }
 function createToggleButton()
@@ -28,11 +36,12 @@ function createToggleButton()
   var btn = document.createElement("button");
   btn.id = "toggleScoresBtn";
   btn.appendChild(document.createTextNode("Toggle Scores!!"));
-  btn.onclick = toggleScores
-  btn.style.backgroundColor = "#3db4f2";
+  btn.onclick = toggleScoresAndSaveState;
+  btn.style.backgroundColor = "#182a34";
   btn.style.color = "white";
   btn.style.borderRadius = "6px";
   targetNode.appendChild(btn);
+  toggleBtn = btn;
 
 }
 function createStyleNode()
@@ -48,6 +57,12 @@ function Init_ScoreToggle()
   createStyleNode();
   createToggleButton();
   console.log("Toggle Anilist Scores: Button initilized");
+  console.log("Syncing button state with localStorage...");
+  if(state)
+  {
+    styleNode.innerHTML = ".score:not(.form) {display:none!important;}";
+    toggleBtn.style.backgroundColor = "#3db4f2";
+  }
 }
 
 function CheckForTarget(mutationList, observer)
